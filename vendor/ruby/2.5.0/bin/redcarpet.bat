8b6b876f0a713c29a2215098207d6bb99c7ab0f8
Ricardo Morales Paredes <ricardo.morales.paredes.contractor@bbva.com>
2019-02-20 13:08:59 -0600

    reverse of reverses

---
+++ vendor/ruby/2.5.0/bin/redcarpet.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Users\rdme\tools\Ruby25-x64\bin\ruby.exe" "C:/Users/rdme/fireflyartstudio.github.io/vendor/ruby/2.5.0/bin/redcarpet" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Users\rdme\tools\Ruby25-x64\bin\ruby.exe" "%~dpn0" %*
